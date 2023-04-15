77c9b79de62647358e8b2b3b52ab99473e10a00a
elG5 <punch6@ya.ru>
2023-04-15 15:49:51 +0300

    Добавлена латиница для шифра Плейфера

--- src/gui/SlectMenuItem.java
+++ src/gui/SlectMenuItem.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 
 public class SlectMenuItem extends JComboBox<String> {
     ArrayList<Character> massNum =  new ArrayList<>(Arrays.asList('1','2','3','4','5','6','7','8','9'));
+    private int iterSelect = 0;
     private static JComboBox<String> selectMenu;
     private MainPanel mp;
     public SlectMenuItem(MainPanel mp){
@@ -34,12 +35,14 @@ public class SlectMenuItem extends JComboBox<String> {
                 JComboBox<String> box = (JComboBox<String>) e.getSource();
                 String selcted = (String) box.getSelectedItem();
 
-                if (selcted.equals(items[0])||selcted.equals(items[5])){
+                if (selcted.equals(items[0])){
                     PanelKey.labelKeyPanel.setText("Введите ключевое слово -> ");
                     PanelKey.areaKeyPanel.setText("Введите ключ");
                     PanelKey.areaKeyPanel.getAccessibleContext().setAccessibleDescription("Здесь введите ключ шифровкию. Может быть любая фраза.");
                     PanelKey.areaKeyPanel.setEnabled(true);
                     PanelKey.iter = 0;
+                    PanelKey.removeEnRu();
+                    iterSelect = 0;
                 }
                 if (selcted.equals(items[1])){
                     PanelKey.labelKeyPanel.setText("Введите число смещения до 33 -> ");
@@ -47,10 +50,25 @@ public class SlectMenuItem extends JComboBox<String> {
                     PanelKey.areaKeyPanel.getAccessibleContext().setAccessibleDescription("Здесь введите число сдвига. Только число до 33");
                     PanelKey.areaKeyPanel.setEnabled(true);
                     PanelKey.iter = 0;
+                    PanelKey.removeEnRu();
+                    iterSelect = 0;
                 }
                 if (selcted.equals(items[3])||selcted.equals(items[2])||selcted.equals(items[4])){
                     PanelKey.labelKeyPanel.setText("Ничего писать не надо -> ");
                     PanelKey.areaKeyPanel.setEnabled(false);
+                    PanelKey.iter = 0;
+                    PanelKey.removeEnRu();
+                    iterSelect = 0;
+                }
+                if (selcted.equals(items[5])){
+                    PanelKey.labelKeyPanel.setText("Введите ключевое слово -> ");
+                    PanelKey.areaKeyPanel.setText("Введите ключ");
+                    PanelKey.areaKeyPanel.getAccessibleContext().setAccessibleDescription("Здесь введите ключ шифровкию. Может быть любая фраза.");
+                    PanelKey.areaKeyPanel.setEnabled(true);
+                    if(iterSelect<1){
+                        PanelKey.addEnRu();
+                        iterSelect++;
+                    }
                     PanelKey.iter = 0;
                 }
             }
